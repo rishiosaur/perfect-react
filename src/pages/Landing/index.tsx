c4990d4a77d89b1a8ec6a55fab8fe635d4ecd46e
Rishi Kothari <itsrishikothari@gmail.com>
2020-09-05 21:23:02 -0400

    landing page

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { AppLayout } from '../../layouts'
+
+const LandingPage: React.FC = () => (
+	<AppLayout>
+		<h1>Hi there!</h1>
+		<p>Hello World!</p>
+	</AppLayout>
+)
+
+export default LandingPage
