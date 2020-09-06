7c4f94602af4a024a5d79b48946f46eb4d5fce97
Rishi Kothari <itsrishikothari@gmail.com>
2020-09-05 21:22:49 -0400

    index

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+import App from './App'
+
+ReactDOM.render(<App />, document.querySelector('#container'))
+
+if (module && module.hot) {
+	module.hot.accept()
+
+	module.hot.addStatusHandler((status) => {
+		if (status === 'prepare') console.clear()
+	})
+}
